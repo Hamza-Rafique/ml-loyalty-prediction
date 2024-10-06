cdc492188ff14d5cd730a61c7da3bf4a4780d68d
Hamza <hamza.rafique@iga.gov.bh>
2024-10-06 10:35:56 +0300

    loyalty predication example task

--- backend/model.py
+++ backend/model.py
@@ -3,17 +3,13 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 
-# Load dataset
 data = pd.read_csv('dataset.csv')
 
-# Features and target
 X = data.drop('Loyalty_Score', axis=1)
 y = data['Loyalty_Score']
 
-# Train model
 clf = RandomForestClassifier()
 clf.fit(X, y)
 
-# Save model to disk
 with open('model.pkl', 'wb') as f:
     pickle.dump(clf, f)
